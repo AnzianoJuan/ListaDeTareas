eb4c53dcdb91aef54ec786e53a4e5563d66403fc
Juan anziano <juanianziano22@gmail.com>
2025-01-13 17:29:41 -0300

    Comienzo de la app, solo tengo el modelo de dominio, falta seguir haciendo la parte de presentacion y la parte de negocio.... pero antes de eso tengo que terminar la DB asi el cruce de las tareas con la autenticacion de los usuarios no quedan huerfanas

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int UsuarioID { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contra { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
